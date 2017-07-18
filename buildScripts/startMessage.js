e0fbbd94aa17976c2e10ec70d3a44f5ab71c9094
Wojciech Mrózek <wojtek229@o2.pl>
2017-07-18 16:02:57 +0200

    Configured babel

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
-var chalk = require('chalk'); //biblioteka umozliwiajaca zmiane kolorow w konsoli
+import chalk from 'chalk'; //biblioteka umozliwiajaca zmiane kolorow w konsoli
 
 console.log(chalk.green('Starting app in dev mode..'));
